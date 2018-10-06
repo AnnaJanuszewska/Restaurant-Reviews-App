277ed6990569bdac9681a3f2a58670c15c968f5a
Anna Januszewska <38108441+AnnaJanuszewska@users.noreply.github.com>
2018-10-05 22:12:58 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Restaurant-Reviews-App
+# Restaurant Reviews App
+
+
+## Project Overview
+This Restaurant Review Application helps users find information about restaurants in NYC: location, cuisine, opening hours, clients reviews and even some pictures. 
+App is full responsive so users can use it on every device regardless of a sceen size.
+
+
+## Installation
+To run the application you have to clone or download this Restaurant-Reviews-App folder. When you download it on your computer, unzipp it. In this folder, start up a simple HTTP server to serve up the site files on your local computer. Open an index.html file on http://localhost:8000/
---
+++ sw.js
@@ -0,0 +1,62 @@
+/*
+During working on this project I was using resources listed below:
+
+-- https://matthewcranford.com/restaurant-reviews-app-walkthrough-part-4-service-workers/
+-- https://developers.google.com/web/fundamentals/primers/service-workers/
+-- https://www.youtube.com/watch?v=2PY733qFR3A&t=2s&list=PLKC17wty6rS1XVZbRlWjYU0WVsIoJyO3s&index=7
+
+*/
+
+
+const cacheFiles = [
+    "/",
+    "/index.html",
+    "/restaurant.html",
+    "/css.styles.css",
+    "/js/dbhelper.js",
+    "/js/main.js",
+    "/js/restaurant_info.js",
+    "/data/restaurants.json",
+    "/img/1.jpg",
+    "/img/2.jpg",
+    "/img/3.jpg",
+    "/img/4.jpg",
+    "/img/5.jpg",
+    "/img/6.jpg",
+    "/img/7.jpg",
+    "/img/8.jpg",
+    "/img/9.jpg",
+    "img/10.jpg"
+];
+
+self.addEventListener("install", function(e) {
+    e.waitUntil(
+        caches.open("v1").then(function(cache) {
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+self.addEventListener("fetch", function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            if (response) {
+                console.log("Found ", e.request, " in cache");
+                return response;
+            } else {
+                console.log("Could not find ", e.request, " in cache, FETCHING!");
+                return fetch(e.request)
+                .then(function(response) {
+                    const clonedResponse = response.clone();
+                    caches.open("v1").then(function(cache) {
+                        cache.put(e.request, clonedResponse);
+                    })
+                    return response;
+                })
+                .catch(function(err) {
+                    console.log(err);
+                });
+            }
+        })
+    );
+});
